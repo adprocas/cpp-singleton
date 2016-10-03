ef141c384aa4c23540bd7375d71e20a0e1a6ebcf
adprocas <adam@leathorn.com>
2016-10-02 21:49:55 -0400

    Change test order and add more information

--- Singleton.cpp
+++ Singleton.cpp
@@ -2,46 +2,12 @@
 #include <iostream>
 
 int main() {
-	std::cout << "#### ob1 ##### ---- New Example Object using SingletonObject" << endl;
-	ExampleObject* o1 = &SingletonObject::getSingleton();
-	o1->increment();
-	o1->increment();
-	o1->increment();
-	o1->printMessage();
+	cout << "Testing with threads - printing is not consistent, but increment is, as it " << endl;
+	cout << "is using CRITICAL_SECTION, which is only available in Windows currently" << endl;
 
-	std::cout << "#### ob2 ##### ---- New Example Object using SingletonObject" << endl;
-	ExampleObject* o2 = &SingletonObject::getSingleton();
-	o2->increment();
-	o1->printMessage();
-
-	std::cout << "#### ob3 ##### ---- New Example Object using SingletonObject" << endl;
-	ExampleObject* o3 = &SingletonObject::getSingleton();
-	o3->increment();
-	o3->increment();
-	o3->increment();
-	o3->increment();
-	o3->increment();
-	o3->increment();
-	o1->printMessage();
-
-	std::cout << "#### ob4 ##### ---- New Example Object using SingletonObject" << endl;
-	ExampleObject* o4 = &SingletonObject::getSingleton();
-	o4->increment();
-	o4->increment();
-	o4->increment();
-	o1->printMessage();
-
-	std::cout << endl << "Print Message for all 4 objects (ob1, ob2, ob3, ob4)" << endl;
-
-	o1->printMessage();
-	o2->printMessage();
-	o3->printMessage();
-	o4->printMessage();
-
-
-	cout << "Testing with threads" << endl;
 	vector<thread> threads;
 	int const THREAD_NUM = 100;
+
 	for (int i = 1; i <= THREAD_NUM; ++i) {
 		threads.push_back(
 			thread(
@@ -49,17 +15,46 @@ int main() {
 			)
 		);
 	}
-	
+
 	cout << "joining threads....." << endl;
-	
+
 	for (thread& t : threads) {
 		t.join();
 	}
 
-	cout << "Ending program - printing out for the four objects again - final number should be 113" << endl;
+	std::cout << "#### ob1 ##### ---- New Example Object using SingletonObject" << endl;
+	ExampleObject* ob1 = &SingletonObject::getSingleton();
+	ob1->increment();
+	ob1->increment();
+	ob1->increment();
+	ob1->printMessage();
 
-	o1->printMessage();
-	o2->printMessage();
-	o3->printMessage();
-	o4->printMessage();
+	std::cout << "#### ob2 ##### ---- New Example Object using SingletonObject" << endl;
+	ExampleObject* ob2 = &SingletonObject::getSingleton();
+	ob2->increment();
+	ob1->printMessage();
+
+	std::cout << "#### ob3 ##### ---- New Example Object using SingletonObject" << endl;
+	ExampleObject* ob3 = &SingletonObject::getSingleton();
+	ob3->increment();
+	ob3->increment();
+	ob3->increment();
+	ob3->increment();
+	ob3->increment();
+	ob3->increment();
+	ob1->printMessage();
+
+	std::cout << "#### ob4 ##### ---- New Example Object using SingletonObject" << endl;
+	ExampleObject* ob4 = &SingletonObject::getSingleton();
+	ob4->increment();
+	ob4->increment();
+	ob4->increment();
+	ob1->printMessage();
+
+	std::cout << endl << "Print Message for all 4 objects (ob1, ob2, ob3, ob4) -- final number should be 113" << endl;
+
+	ob1->printMessage();
+	ob2->printMessage();
+	ob3->printMessage();
+	ob4->printMessage();
 }
